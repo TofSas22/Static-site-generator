fe0fcc1850ece1f6564ffc4065bafaf3e2a2fb98
TofSas22 <tsasman13@gmail.com>
2025-03-26 15:39:26 +0200

    created parent node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,9 +1,9 @@
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
-        self.tag = tag
-        self.value = value
-        self.children = children
-        self.props = props
+        self.tag = tag # html tag
+        self.value = value # str
+        self.children = children # list of objects
+        self.props = props # dict
 
     def to_html(self):
         raise NotImplementedError("to_html method not implemented")
@@ -32,3 +32,21 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("invalid HTML: missing tag")
+        if self.children == None:
+            raise ValueError("invalid HTML: no children found")
+        children = ""
+        for child in self.children:
+            children += child.to_html()
+        
+        return f"<{self.tag}>{children}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
